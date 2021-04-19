cf413d1a7d075010268828b0d7a0928fd1b4250f
ashwin27santhosh <82137112+ashwin27santhosh@users.noreply.github.com>
2021-04-19 09:22:33 +0530

    Add files via upload

---
+++ currentAcc.java
@@ -0,0 +1,27 @@
+package Lab4;
+
+public class CurrentAccount extends Account{
+    private double overDraftLimit;
+
+	public double getOverDraftLimit() {
+		return overDraftLimit;
+	}
+
+	public void setOverDraftLimit(double overDraftLimit) {
+		this.overDraftLimit = overDraftLimit;
+	}
+
+	public CurrentAccount() {
+		super();
+	}
+
+	@Override
+	public void withDraw(double amount) {
+		// TODO Auto-generated method stub
+		if((super.getBalance() + amount) > overDraftLimit) {
+			System.out.println("Over Draft Limit has been exceeded.");
+		}
+		 
+	}
+    
+}
---
+++ user.java
@@ -0,0 +1,26 @@
+package Lab4;
+
+public class user {
+    private String name;
+    private float age;
+
+	public user(String name, float age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) { 
+		this.name = name;
+	}
+	public float getAge() {
+		return age;
+	}
+	public void setAge(float age) {
+		this.age = age;
+	}
+	
+     
+}
